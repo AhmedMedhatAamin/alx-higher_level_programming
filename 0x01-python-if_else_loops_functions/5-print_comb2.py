4b33372172b906dfad43ad9d851d5f4feda9e1b4
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-11-28 12:11:32 -0800

    ahmed

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    print("{:02}".format(i), end=", " if i < 99 else "\t")
+try:
+    for i in range(0, 100):
+        print("{:02}".format(i), end=", " if i < 99 else "\t")
+except Exception as e:
+    print(f"[Error]: {e}")
