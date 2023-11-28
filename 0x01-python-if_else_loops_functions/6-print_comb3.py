bc75d5480030137b14822dc7c9eb1cfba49ce645
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-11-28 15:16:16 -0800

    ahmed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit + 1, 10):
-        print("{:d}{:d}".format(tens_digit, ones_digit), end=", " if ones_digit < 9 else "")
+for i in range(0, 10):
+    for j in range(1, 10):
+        if i >= j:
+            continue
+        elif i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}, ".format(i, j), end="")
