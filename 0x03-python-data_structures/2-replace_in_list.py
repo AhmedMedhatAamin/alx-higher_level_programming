b6e69a275bd05f3c2a4fa7a97814c912744c3e00
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-12-02 13:47:03 -0800

    ahmed

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, new_element):
+    my_len_list = len(my_list)
+    if idx < 0 or idx >= my_len_list:
+        return None
+    else:
+        my_list[idx] = new_element
+        return my_list
