66db8681951b2533579fb7a2bf8c43f2ef10f7a8
root <ahmedmedhat.ahmed011@gmail.com>
2024-02-14 11:15:37 -0800

    ahmed

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,10 @@
 -- creat databasea
-create database hbtn_0c_0;
+
+
+-- cheak if the database exist
+
+use hbtn_0c_0;
+
+--IF NOT EXIST THE DATA BASE WILL BE CREATED
+
+create database if not EXISTS hbtn_0c_0;
